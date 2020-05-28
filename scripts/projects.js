dca58ac8902a0f9831f8c5c3c51f3682da052612
timleevt <timleemu@vt.edu>
2020-05-28 00:27:11 -0400

    changed colors, removed unnecessary info/files, set projects to TBA

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,11 +1,6 @@
-function showLoZ() {
-    let projectDcElement = document.getElementById("project_loz");
-    if(projectDcElement.style.display === "none") { projectDcElement.style.display = "block"; }
-    else { projectDcElement.style.display = "none"; }
-}
-
-function showDeClieu() {
+function showProject() {
     let projectDcElement = document.getElementById("project_dc");
     if(projectDcElement.style.display === "none") { projectDcElement.style.display = "block"; }
     else { projectDcElement.style.display = "none"; }
 }
+
