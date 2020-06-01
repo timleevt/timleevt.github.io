aa8528d65c2f1fb9b433fe93fd81aba81b8ace93
timleevt <timleemu@vt.edu>
2020-06-01 12:01:32 -0400

    put placeholder for current project

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,5 +1,5 @@
 function showProject() {
-    let projectDcElement = document.getElementById("project_dc");
+    let projectDcElement = document.getElementById("project_tpc");
     if(projectDcElement.style.display === "none") { projectDcElement.style.display = "block"; }
     else { projectDcElement.style.display = "none"; }
 }
